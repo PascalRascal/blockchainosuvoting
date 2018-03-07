08381205d6388326cbf5b14b700d4756733ccfb1
pascalrascal <marchetti.14@osu.edu>
2018-03-07 13:32:40 -0500

    Fixed Migration

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.2;
 
 /// @title Voting with delegation.
 contract Election {
--- migrations/1519959304_ballot.js
+++ migrations/1519959304_ballot.js
@@ -3,5 +3,5 @@ var Election = artifacts.require("./Election.sol");
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
-  deployer.deploy(Election, ["Facey McPresident", "Bob Dole", "Marcus Veepson", "Blaise", "Charlie Lee", "Satoshi", "Bitbean Bean", "Vitalik", "Carlos"], [2,3,2,2])
+  deployer.deploy(Election, ["Facey McPresident", "Bob Dole", "Marcus Veepson", "Charlie Lee", "Satoshi", "Bitbean Bean", "Vitalik", "Carlos"], [2,2,2,2])
 };
